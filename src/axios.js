52e266f234aa846598a1259442fbe76563c91c47
Alex Muscina <alexandrumuscina@gmail.com>
2021-04-14 01:18:36 +0300

    commit

--- src/axios.js
+++ src/axios.js
@@ -1,15 +1,6 @@
 import axios from "axios";
-<<<<<<< HEAD
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3/"
-=======
-
-const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
-  headers: {
-    "Content-Type": "application/json"
-  },
->>>>>>> 48986b3b09a066acc9a878706e7e986a0e7c79ae
 });
 
 export default instance;
