e6d99e294ec27774a94219ebe8fc3a75656f5f00
Alexandru Muscina <alexandrumuscina@gmail.com>
2021-04-14 00:23:29 +0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   headers: {
-    'Authorization': `Bearer ${process.env.REACT_APP_TMDB_KEY}`,
-    'Content-Type' : 'application/json;charset=utf-8'
+    Authorization: `Bearer ${process.env.REACT_APP_TMDB_KEY}`,
+    "Content-Type": "application/json"
   },
 });
 
