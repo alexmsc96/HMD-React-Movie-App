48986b3b09a066acc9a878706e7e986a0e7c79ae
Alexandru Muscina <alexandrumuscina@gmail.com>
2021-04-14 00:55:41 +0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_TMDB_KEY}`,
     "Content-Type": "application/json"
   },
 });
